ffbe8ba325a5764fb58b027cbd5933bdf4be4265
fluke.jessada.s <fluke.jessada.s@gmail.com>
2021-03-10 09:38:37 +0700

    Finally Hooks All of them.

--- components/ChartRender.js
+++ components/ChartRender.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { HorizontalBar } from 'react-chartjs-2'
 import { useChartContext } from '../context/ChartContext'
 
-const ChartData = props => {
-    return(
-        <HorizontalBar data="" height={1000} />
+const staticDataset = {
+    labels: [],
+    datasets: [
+        {
+            label: 'infected',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            barThickness: 12,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: []
+        }
+    ]
+}
+
+const ChartRender = () => {
+    const { chartData, isBegin, setIsBegin, date } = useChartContext()
+    const [dataset, setDataset] = useState(staticDataset)
+
+    const onStart = () => setIsBegin(true)
+
+    const onStop = () => setIsBegin(false)
+
+    const onDatasetChange = useCallback(() => {
+        let tempCountry = []
+        let tempInfected = []
+        let newDataset = {
+            ...staticDataset
+        }
+
+        chartData.map(val => {
+            tempCountry.push(val.country)
+            tempInfected.push(val.infected)
+        })
+
+        newDataset.labels = tempCountry
+        newDataset.datasets[0].data = tempInfected
+
+        console.log(newDataset)
+        setDataset(newDataset)
+    },
+        [chartData],
+    )
+
+    useEffect(() => {
+        onDatasetChange()
+    }, [chartData, date])
+
+    return (
+        <>
+            <h1>Covid-19 Historical Data</h1>
+            <button onClick={onStart} disabled={isBegin}>Start/Resume</button>
+            <button onClick={onStop} disabled={!isBegin}>Stop</button>
+            <p>{date}</p>
+            <HorizontalBar data={dataset} height={1000} />
+        </>
     )
 }
+
+export default ChartRender
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
 import Head from 'next/head'
 import useHistoricalData from '../actions/useHistoricalData'
 import ChartData from '../components/ChartData'
+import ChartRender from '../components/ChartRender'
+import ChartContext from '../context/ChartContext'
 
 export default function Home() {
   const [isBegin, setIsBegin, date, chartData] = useHistoricalData()
 
-  console.log(chartData)
+  const valuesContext = {
+    chartData,
+    isBegin,
+    setIsBegin,
+    date
+  }
 
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Covid</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Covid-19 Historical Data</h1>
-      <button onClick={() => setIsBegin(true)} disabled={isBegin}>Start/Resume</button>
-      <button onClick={() => setIsBegin(false)} disabled={!isBegin}>Stop</button>
-      <p>{date}</p>
-      <ChartData item={chartData} />
+      <ChartContext.Provider value={valuesContext}>
+        <ChartRender />
+      </ChartContext.Provider>
     </div>
   )
 }
